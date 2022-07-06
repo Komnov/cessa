b7ec4b251ff6eb37bac42339c2aeb8bc3412581e
/usr/bin/vim <support@beget.ru>
2022-07-06 16:10:18 +0300

    slider

---
+++ admin/language/en-gb/extension/module/ilmflexslider.php
@@ -0,0 +1,35 @@
+<?php
+// Heading
+$_['heading_title']    = 'Ilm Flexslider';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified Ilm Flexslider module!';
+$_['text_edit']        = 'Edit Ilm Flexslider Module';
+$_['text_true']        = 'True';
+$_['text_false']       = 'False';
+$_['button_save_stay'] = 'Save and Stay';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_name']       = 'Module Name';
+$_['entry_banner']     = 'Banner';
+$_['entry_slideshowspeed']  = 'Slideshow Speed';
+$_['entry_animationspeed']  = 'Animation Speed';
+$_['entry_animationloop']   = 'Animation Loop';
+$_['entry_touch']           = 'Touch';
+$_['entry_pausehover']      = 'Pause on Hover';
+$_['entry_controlnav']      = 'Control Navigation';
+$_['entry_directionnav']    = 'Direction Navigation';
+$_['entry_limit']           = 'Item Per View';
+$_['entry_itemwidth']       = 'Item Width';
+$_['entry_itemmargin']      = 'Item Margin';
+    
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Ilm Flexslider module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_slideshowspeed']  = 'Slideshow Speed required!';
+$_['error_animationspeed']  = 'Animation Speed required!';
+$_['error_limit']           = 'Limit required!';
+$_['error_itemwidth']       = 'Item Width required!';
+$_['error_itemmargin']      = 'Item Margin required!';
