4585267f1bd35db54f2cf34c3328a289db56c63e
/usr/bin/vim <support@beget.ru>
2023-10-08 15:00:16 +0300

    new module delivery and edit base

--- system/library/db/mysqli.php
+++ system/library/db/mysqli.php
@@ -1,64 +1,89 @@
 <?php
 namespace DB;
+
 final class MySQLi {
-	private $connection;
+    private $hostname;
+    private $username;
+    private $password;
+    private $database;
+    private $port;
+    private $connection;
 
-	public function __construct($hostname, $username, $password, $database, $port = '3306') {
-		$this->connection = new \mysqli($hostname, $username, $password, $database, $port);
+    public function __construct($hostname, $username, $password, $database, $port = '3306') {
+        $this->hostname = $hostname;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+        $this->port = $port;
 
-		if ($this->connection->connect_error) {
-			throw new \Exception('Error: ' . $this->connection->connect_error . '<br />Error No: ' . $this->connection->connect_errno);
-		}
+        $this->connect();
+    }
 
-		$this->connection->set_charset("utf8");
-		$this->connection->query("SET SQL_MODE = ''");
-		$this->connection->query("SET SESSION sql_mode = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION'");
-	}
+    private function connect() {
+        $this->connection = new \mysqli($this->hostname, $this->username, $this->password, $this->database, $this->port);
 
-	public function query($sql) {
-		$query = $this->connection->query($sql);
+        if ($this->connection->connect_error) {
+            throw new \Exception('Error: ' . $this->connection->connect_error . '<br />Error No: ' . $this->connection->connect_errno);
+        }
 
-		if (!$this->connection->errno) {
-			if ($query instanceof \mysqli_result) {
-				$data = array();
+        $this->connection->set_charset("utf8");
+        $this->connection->query("SET SQL_MODE = ''");
+        $this->connection->query("SET SESSION sql_mode = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION'");
+        
+        // Увеличьте время ожидания соединения до 300 секунд (или другого значения по вашему усмотрению)
+        $this->connection->options(MYSQLI_OPT_CONNECT_TIMEOUT, 300);
+    }
 
-				while ($row = $query->fetch_assoc()) {
-					$data[] = $row;
-				}
+    public function query($sql) {
+        // Переподключитесь к серверу, если соединение потеряно
+        if (!$this->connection->ping()) {
+            $this->connect();
+        }
 
-				$result = new \stdClass();
-				$result->num_rows = $query->num_rows;
-				$result->row = isset($data[0]) ? $data[0] : array();
-				$result->rows = $data;
+        $query = $this->connection->query($sql);
 
-				$query->close();
+        if (!$this->connection->errno) {
+            if ($query instanceof \mysqli_result) {
+                $data = array();
 
-				return $result;
-			} else {
-				return true;
-			}
-		} else {
-			throw new \Exception('Error: ' . $this->connection->error  . '<br />Error No: ' . $this->connection->errno . '<br />' . $sql);
-		}
-	}
+                while ($row = $query->fetch_assoc()) {
+                    $data[] = $row;
+                }
 
-	public function escape($value) {
-		return $this->connection->real_escape_string($value);
-	}
-	
-	public function countAffected() {
-		return $this->connection->affected_rows;
-	}
+                $result = new \stdClass();
+                $result->num_rows = $query->num_rows;
+                $result->row = isset($data[0]) ? $data[0] : array();
+                $result->rows = $data;
 
-	public function getLastId() {
-		return $this->connection->insert_id;
-	}
-	
-	public function connected() {
-		return $this->connection->ping();
-	}
-	
-	public function __destruct() {
-		$this->connection->close();
-	}
+                $query->close();
+
+                return $result;
+            } else {
+                return true;
+            }
+        } else {
+            throw new \Exception('Error: ' . $this->connection->error  . '<br />Error No: ' . $this->connection->errno . '<br />' . $sql);
+        }
+    }
+
+    public function escape($value) {
+        return $this->connection->real_escape_string($value);
+    }
+    
+    public function countAffected() {
+        return $this->connection->affected_rows;
+    }
+
+    public function getLastId() {
+        return $this->connection->insert_id;
+    }
+    
+    public function connected() {
+        return $this->connection->ping();
+    }
+    
+    public function __destruct() {
+        $this->connection->close();
+    }
 }
+?>
